e6f54a64448154d1318d191d9dd46b5cb4ab273d
Kirin Patel <kirinpatel@gmail.com>
2018-07-17 21:47:43 -0400

    Updated functions to now store all data from meh API

--- functions/index.js
+++ functions/index.js
@@ -1,60 +1,58 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
+const axios = require("axios");
+const functions = require("firebase-functions");
 admin.initializeApp(functions.config().firebase);
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-var db = admin.database();
-var ref = db.ref();
+let ref = admin.database().ref();
 
 exports.updateItem = functions.https.onRequest((request, response) => {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() { 
-        if (this.readyState == 4 && this.status == 200) { 
-            var res = JSON.parse(xhttp.responseText);
-            
-            ref.child('info').set({
-                id: res['deal']['id'],
-                title: res['deal']['title'],
-                description: res['deal']['features'],
-                photos: res['deal']['photos'],
-                items: res['deal']['items']
-            });
-            ref.child('settings').set({
-                accentColor: res['deal']['theme']['accentColor'],
-                backgroundColor: res['deal']['theme']['backgroundColor'],
-                foreground: res['deal']['theme']['foreground']
-            });
-        }
-    };
-    xhttp.open('GET', 'https://api.meh.com/1/current.json?apikey=KEY', true);
-    xhttp.send();
-    response.send('Updated.');
+  return ref.child("API_KEY").once("value", (snapshot) => {
+    const API_KEY = snapshot.val();
+    return axios.get(`https://api.meh.com/1/current.json?apikey=${API_KEY}`).then((res) => {
+      ref.child("deal").set(res.data.deal);
+      ref.child("info").set({
+        id: res.data.deal.id,
+        title: res.data.deal.title,
+        description: res.data.deal.features,
+        photos: res.data.deal.photos,
+        items: res.data.deal.items
+      });
+      ref.child("settings").set({
+        accentColor: res.data.deal.theme.accentColor,
+        backgroundColor: res.data.deal.theme.backgroundColor,
+        foreground: res.data.dealtheme.foreground
+      });
+      response.send("Completed.");
+      return true;
+    }).catch((err) => {
+      console.error("Error fetching meh data...", err);
+      return err;
+    })
+  });
 });
 
-exports.sendDealUpdate = functions.database.ref('/info').onUpdate(event => {
-    const info = event.data.val();
-    const tokens = [];
-    var payload = {
-            notification: {
-                title: 'Check out this new deal!',
-                body: info.title,
-                content_available: 'true'
-            }
-    };
-    
-    admin.database().ref(`/notifications`).once('value', (snapshot) => {
-        snapshot.forEach(function (token) {
-            if (token.val()) {
-                admin.messaging().sendToDevice(token.key, payload).then(function (response) {
-                    console.log("Successfully sent message:", JSON.stringify(response));
-                }).catch(function (error) {
-                    console.log("Error sending message:", JSON.stringify(error));
-                });
-            }
-        });
-        
-        return true;
+exports.sendDealUpdate = functions.database.ref("deal").onUpdate(event => {
+  const deal = event.data.val();
+  const tokens = [];
+  const payload = {
+    notification: {
+      title: "Check out this new deal!",
+      body: deal.title,
+      content_available: "true"
+    }
+  };
+
+  return admin.database().ref("/notifications").once("value", (snapshot) => {
+    snapshot.forEach(function (token) {
+      tokens.push(token.val());
     });
-    
-    return 'No devices.';
-})
+
+    return admin.messaging().sendToDevice(tokens, payload).then((res) => {
+      console.log("Successfully sent message:", JSON.stringify(res));
+      return true;
+    }).catch((err) => {
+      console.log("Error sending message:", JSON.stringify(error));
+      return err;
+    });
+  });
+});
