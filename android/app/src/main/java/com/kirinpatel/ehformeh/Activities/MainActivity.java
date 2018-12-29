cb7a255adb0a088e87bd3a03d4a5bcb76f0eec4c
Kirin Patel <kirinpatel@gmail.com>
2018-12-29 14:53:21 -0500

    Reset android app, dropped SlidingDrawer

--- android/app/src/main/java/com/kirinpatel/ehformeh/Activities/MainActivity.java
+++ android/app/src/main/java/com/kirinpatel/ehformeh/Activities/MainActivity.java
@@ -4,96 +4,37 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Point;
-import android.net.Uri;
 import android.support.constraint.ConstraintLayout;
-import android.support.v4.view.PagerTabStrip;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseError;
-import com.kirinpatel.ehformeh.Adapters.BottomSheetPagerAdapter;
 import com.kirinpatel.ehformeh.R;
 import com.kirinpatel.ehformeh.utils.Deal;
 import com.kirinpatel.ehformeh.utils.DealLoader;
 import com.kirinpatel.ehformeh.utils.DealLoaderInterface;
-import com.kirinpatel.ehformeh.utils.Item;
-import com.pierfrancescosoffritti.slidingdrawer.SlidingDrawer;
-
-import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Deal deal;
     private boolean hasAnimated = false;
 
-    private SlidingDrawer mainLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainLayout = findViewById(R.id.mainLayout);
-        setupSlidingView();
-        setupCurrentDealListener();
-    }
-
-    private void setupViewPager() {
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        BottomSheetPagerAdapter adapter = new BottomSheetPagerAdapter(deal, getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-
-        Button buyButton = findViewById(R.id.buyDealButton);
-        buyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(deal.getURL().toString()));
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void setupSlidingView() {
-        mainLayout.setDragView(findViewById(R.id.slidableViewContent));
-        findViewById(R.id.dealInfoTextView).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mainLayout.slideTo(mainLayout.getState() == SlidingDrawer.EXPANDED ? 0 : 1);
-            }
-        });
-        calculateScreenHeight();
-    }
-
-    private void calculateScreenHeight() {
-        ConstraintLayout contentView = findViewById(R.id.contentView);
-        ViewGroup.LayoutParams params = contentView.getLayoutParams();
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        params.height = (int) (size.y * .85);
-
-        contentView.setLayoutParams(new LinearLayout.LayoutParams(params));
+        setupDealListener();
     }
 
     private void animateStart() {
         if (deal != null && !hasAnimated) {
             hasAnimated = true;
             final ConstraintLayout loadingBackground = findViewById(R.id.loadingLayout);
-            final ConstraintLayout slidingView = findViewById(R.id.slidableViewContent);
+            final ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
             final TextView loadingTitle = findViewById(R.id.titleTextView);
 
             ValueAnimator backgroundColorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(),
@@ -132,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
                     loadingBackground.setVisibility(View.GONE);
                     mainLayout.setBackgroundColor(Color.parseColor(deal.getTheme().getBackgroundColor()));
                     mainLayout.setVisibility(View.VISIBLE);
-                    slidingView.setBackgroundColor(Color.parseColor(deal.getTheme().getAccentColor()));
                 }
 
                 @Override
@@ -158,15 +98,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setupCurrentDealListener() {
+    private void setupDealListener() {
         DealLoader loader = new DealLoader(new DealLoaderInterface() {
             @Override
             public void dealLoaded(Deal loadedDeal) {
                 deal = loadedDeal;
-
                 animateStart();
-                updateUIWithCurrentDeal();
-                setupViewPager();
             }
 
             @Override
@@ -184,37 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
         loader.loadCurrentDeal();
-    }
-
-    private void updateUIWithCurrentDeal() {
-        if (deal != null) {
-            // Title
-            TextView dealTitle = findViewById(R.id.dealTitleTextView);
-            dealTitle.setText(deal.getTitle());
-            dealTitle.setTextColor(Color.parseColor(deal.getTheme().getAccentColor()));
-
-            // Price
-            TextView dealPrice = findViewById(R.id.dealPriceTextView);
-            Float minPrice = Float.MAX_VALUE;
-            Float maxPrice = Float.MIN_VALUE;
-            for (Item item : deal.getItems()) {
-                Float price = item.getPrice();
-                if (price < minPrice) minPrice = price;
-                if (price > maxPrice) maxPrice = price;
-            }
-            String price = "$" + minPrice;
-            if (maxPrice != minPrice) price += " - $" + maxPrice;
-            dealPrice.setText(price);
-
-            // Deal Info
-            TextView dealInfo = findViewById(R.id.dealInfoTextView);
-            dealInfo.setTextColor(Color.parseColor(deal.getTheme().getBackgroundColor()));
-
-            // View Pager
-            PagerTabStrip tabStrip = findViewById(R.id.viewPagerTabs);
-            tabStrip.setTabIndicatorColor(Color.parseColor(deal.getTheme().getBackgroundColor()));
-            tabStrip.setTextColor(Color.parseColor(deal.getTheme().getBackgroundColor()));
-        }
     }
 }
