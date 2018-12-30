ffcd02fb27b993eb48a8920a08e4b4fcd91f1ea9
Kirin Patel <kirinpatel@gmail.com>
2018-12-30 10:59:26 -0500

    Setup basic layout of app

--- android/app/src/main/java/com/kirinpatel/ehformeh/Activities/MainActivity.java
+++ android/app/src/main/java/com/kirinpatel/ehformeh/Activities/MainActivity.java
@@ -1,16 +1,16 @@
 package com.kirinpatel.ehformeh.Activities;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseError;
@@ -18,104 +18,99 @@ import com.kirinpatel.ehformeh.R;
 import com.kirinpatel.ehformeh.utils.Deal;
 import com.kirinpatel.ehformeh.utils.DealLoader;
 import com.kirinpatel.ehformeh.utils.DealLoaderInterface;
+import com.kirinpatel.ehformeh.utils.Item;
 
 public class MainActivity extends AppCompatActivity {
 
     private Deal deal;
-    private boolean hasAnimated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar mehToolbar = findViewById(R.id.mehToolbar);
+        setSupportActionBar(mehToolbar);
+
         setupDealListener();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     private void animateUI() {
-        if (deal != null && !hasAnimated) {
-            hasAnimated = true;
-            final ConstraintLayout loadingBackground = findViewById(R.id.loadingLayout);
-            final ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
-            final TextView loadingTitle = findViewById(R.id.titleTextView);
+        if (deal == null) return;
 
-            ValueAnimator backgroundColorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(),
-                    getResources().getColor(R.color.white),
-                    Color.parseColor(deal.getTheme().getBackgroundColor()));
-            backgroundColorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    loadingBackground.setBackgroundColor((int) animation.getAnimatedValue());
+        final ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
+        final ConstraintLayout bottomSheetLayout = findViewById(R.id.bottomSheetLayout);
+        final ConstraintLayout loadingBackground = findViewById(R.id.loadingLayout);
+        final TextView loadingTitle = findViewById(R.id.titleTextView);
+        final TextView dealTitleTextView = findViewById(R.id.dealTitleTextView);
+        final TextView dealPriceTextView = findViewById(R.id.dealPriceTextView);
+        final Toolbar mehToolbar = findViewById(R.id.mehToolbar);
+
+        ValueAnimator backgroundColorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(),
+                getResources().getColor(R.color.white),
+                Color.parseColor(deal.getTheme().getBackgroundColor()));
+        backgroundColorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                loadingBackground.setBackgroundColor((int) animation.getAnimatedValue());
+                mainLayout.setBackgroundColor((int) animation.getAnimatedValue());
+                mehToolbar.setTitleTextColor((int) animation.getAnimatedValue());
+                Menu menu = mehToolbar.getMenu();
+                for (int i = 0; i < menu.size(); i++) {
+                    Drawable drawable = menu.getItem(i).getIcon();
+                    drawable.mutate();
+                    drawable.setColorFilter((int) animation.getAnimatedValue(), PorterDuff.Mode.SRC_IN);
                 }
-            });
-            backgroundColorAnimation.setDuration(500);
+            }
+        });
+        backgroundColorAnimation.setDuration(500);
 
-            ValueAnimator titleAlphaAnimation = ValueAnimator.ofFloat(1.0f, 0.0f);
-            titleAlphaAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    loadingTitle.setAlpha((float) animation.getAnimatedValue());
-                }
-            });
-            titleAlphaAnimation.setDuration(500);
+        int color = Color.TRANSPARENT;
+        Drawable accent = bottomSheetLayout.getBackground();
+        if (accent instanceof ColorDrawable) color = ((ColorDrawable) accent).getColor();
+        ValueAnimator accentColorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(),
+                color,
+                Color.parseColor(deal.getTheme().getAccentColor()));
+        accentColorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                bottomSheetLayout.setBackgroundColor((int) animation.getAnimatedValue());
+                mehToolbar.setBackgroundColor((int) animation.getAnimatedValue());
+                dealTitleTextView.setTextColor((int) animation.getAnimatedValue());
+                dealPriceTextView.setTextColor((int) animation.getAnimatedValue());
+            }
+        });
+        accentColorAnimation.setDuration(500);
 
-            backgroundColorAnimation.start();
-            titleAlphaAnimation.start();
 
-            titleAlphaAnimation.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationCancel(Animator animation) {
-                    super.onAnimationCancel(animation);
-                }
+        ValueAnimator titleAlphaAnimation = ValueAnimator.ofFloat(1.0f, 0.0f);
+        titleAlphaAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                loadingTitle.setAlpha((float) animation.getAnimatedValue());
+            }
+        });
+        titleAlphaAnimation.setDuration(500);
 
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    super.onAnimationEnd(animation);
+        ValueAnimator mainLayoutAlphaAnimation = ValueAnimator.ofFloat(0.0f, 1.0f);
+        mainLayoutAlphaAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mainLayout.setAlpha((float) animation.getAnimatedValue());
+            }
+        });
+        mainLayoutAlphaAnimation.setDuration(500);
 
-                    loadingBackground.setVisibility(View.GONE);
-                    mainLayout.setBackgroundColor(Color.parseColor(deal.getTheme().getBackgroundColor()));
-                    mainLayout.setVisibility(View.VISIBLE);
-                }
-
-                @Override
-                public void onAnimationRepeat(Animator animation) {
-                    super.onAnimationRepeat(animation);
-                }
-
-                @Override
-                public void onAnimationStart(Animator animation) {
-                    super.onAnimationStart(animation);
-                }
-
-                @Override
-                public void onAnimationPause(Animator animation) {
-                    super.onAnimationPause(animation);
-                }
-
-                @Override
-                public void onAnimationResume(Animator animation) {
-                    super.onAnimationResume(animation);
-                }
-            });
-        } else if (deal != null) {
-            final ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
-
-            int color = Color.TRANSPARENT;
-            Drawable background = mainLayout.getBackground();
-            if (background instanceof ColorDrawable) color = ((ColorDrawable) background).getColor();
-            ValueAnimator backgroundColorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(),
-                    color,
-                    Color.parseColor(deal.getTheme().getBackgroundColor()));
-            backgroundColorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    mainLayout.setBackgroundColor((int) animation.getAnimatedValue());
-                }
-            });
-            backgroundColorAnimation.setDuration(500);
-
-            backgroundColorAnimation.start();
-        }
+        backgroundColorAnimation.start();
+        accentColorAnimation.start();
+        titleAlphaAnimation.start();
+        mainLayoutAlphaAnimation.start();
     }
 
     private void setupDealListener() {
@@ -123,12 +118,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void dealLoaded(Deal loadedDeal) {
                 deal = loadedDeal;
+                setupView();
                 animateUI();
             }
 
             @Override
             public void dealUpdated(Deal updatedDeal) {
                 deal = updatedDeal;
+                setupView();
                 animateUI();
             }
 
@@ -147,6 +144,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupView() {
+        if (deal == null) return;
 
+        TextView dealTitleTextView = findViewById(R.id.dealTitleTextView);
+        TextView dealPriceTextView = findViewById(R.id.dealPriceTextView);
+
+        dealTitleTextView.setText(deal.getTitle());
+
+        // Calculate price to display in dealPriceTextView
+        float minPrice = Float.POSITIVE_INFINITY;
+        float maxPrice = Float.NEGATIVE_INFINITY;
+
+        for (Item item : deal.getItems()) {
+            if (item.getPrice() < minPrice) minPrice = item.getPrice();
+            if (item.getPrice() > maxPrice) maxPrice = item.getPrice();
+        }
+
+        String price = "$" + minPrice;
+        if (deal.getItems().length > 1 && minPrice != maxPrice) price += " - $" + maxPrice;
+        dealPriceTextView.setText(price);
     }
 }
